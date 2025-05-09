b5732aa25ae4e0c3a7f8c796cdfb4524a464e3ed
DingVero <2521248869@qq.com>
2025-05-10 00:24:07 +0800

    尝试ui编写

--- client/uchat-desktop/src/components/Sidebar.tsx
+++ client/uchat-desktop/src/components/Sidebar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../styles/components/Sidebar.css";
 
 const Sidebar: React.FC = () => {
   return (
     <div className="sidebar">
-      <h2>侧边栏</h2>
+      <h2>uchat</h2>
       <ul>
         <li><Link to="/home/friendlist">好友列表</Link></li>
         <li><Link to="/home/messagelist">消息列表</Link></li>
