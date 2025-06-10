9788613d6875d84d10d134208b6c675651dbf0ee
DingVero <2521248869@qq.com>
2025-06-10 18:07:54 +0800

    add ping debug output

--- server/src/handler/mod.rs
+++ server/src/handler/mod.rs
@@ -4,14 +4,25 @@ pub mod group;
 pub mod ws;
 pub mod message;
 
+use axum::response::IntoResponse;
 use axum::Extension;
 use crate::server::AppState;
+use crate::protocol::ServerResponse::GenericResponse;
+use log::debug;
 
 pub async fn handle_request() -> &'static str {
     "Hello, world!"
 }
 
-pub async fn ping(Extension(state): Extension<AppState>) -> String {
+pub async fn ping(Extension(state): Extension<AppState>) -> impl IntoResponse {
+    // 接受http发出的ping请求
+    // 测试request接口与客户端之间通信
+    debug!("响应ping请求");
     let request = state.request.lock().await;
-    request.ping().await
+    let result = request.ping().await;
+    let response = GenericResponse {
+        status: "success".to_string(),
+        message: result,
+    };
+    axum::Json(response).into_response()
 }
